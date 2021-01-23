276e0443d8adbedb2edae57bbe684721918c9ff1
LoganYeu <73202655+LoganYeu@users.noreply.github.com>
2021-01-23 15:25:43 -0500

    Add files via upload

---
+++ Week_2.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Week_2 {
+    public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your order number?");
+        int orderNum = input.nextInt();
+        System.out.println("What is your name?");
+        String name = input.next();
+        System.out.println("How long would you like the desk to be in inches?");
+        int length = input.nextInt();
+        System.out.println("What width would you like the desk to be in inches?");
+        int width = input.nextInt();
+        System.out.println("Would you like mahogany, oak, or pine?");
+        String woodType = input.next();
+        System.out.println("How many drawers would you like?");
+        int numDrawers = input.nextInt();
+
+        int total = 200;
+        int surface = length * width;
+        if (surface > 750){
+            total += 50;
+        }
+        if (woodType.equals("mahogany")){
+            total += 150;
+        }
+        if (woodType.equals("oak")){
+            total += 125;
+        }
+        total += numDrawers * 30;
+        System.out.println(orderNum);
+        System.out.println(name);
+        System.out.println(length);
+        System.out.println(width);
+        System.out.println(woodType);
+        System.out.println(numDrawers);
+        System.out.println("Your total is " + total);
+
+        input.close();
+    }
+
+
+}
